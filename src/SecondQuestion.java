53b41fa2d4c07f30727b5fae8f6aebae38d34eeb
Mbrar3489 <manpreetbrarit401@gmail.com>
2023-02-13 15:52:57 -0500

    Second  Question Done

---
+++ src/SecondQuestion.java
@@ -0,0 +1,17 @@
+public class SecondQuestion {
+
+    public static void main(String[] args){
+
+        // Find the sum of first ten natural numbers using for loop.
+
+        int number =1;
+        int sum=0;
+        while(number<=10){
+           sum= number+sum;
+           number++;
+
+        }
+        System.out.println("Sum of first ten natural numbers is: "+sum);
+
+    }
+}
