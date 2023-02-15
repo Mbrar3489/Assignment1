978de730922962b0f920ac6c2a3de141618d27a4
Mbrar3489 <manpreetbrarit401@gmail.com>
2023-02-15 16:00:12 -0500

    Question five Done

---
+++ src/NegativeNumber.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class NegativeNumber {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int number=sc.nextInt();
+        int sum=0;
+        while(number>=0){
+            sum=number+sum;
+            System.out.print("Enter the number again: ");
+            number=sc.nextInt();
+
+
+        }
+        System.out.print("Sum of all entered numbers is: "+sum);
+
+
+
+    }
+}
