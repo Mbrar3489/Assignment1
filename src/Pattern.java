190b944636adcd9f24fb0f64548652ce259ce009
Mbrar3489 <manpreetbrarit401@gmail.com>
2023-02-17 03:17:28 -0500

    Seventh Question Done

---
+++ src/Pattern.java
@@ -0,0 +1,20 @@
+public class Pattern {
+
+    public static void main(String[] args)
+            // Program to print * pattern using while loop.
+    {
+        int i=0;
+
+        while(i<5)
+        {
+            int j=0;
+            while(j<5) {
+                System.out.print("*");
+                j++;
+            }
+            System.out.println();
+            i++;
+
+        }
+    }
+}
